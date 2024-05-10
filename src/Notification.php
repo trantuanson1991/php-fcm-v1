be195287210cd173194f94b2f631f9630529fe02
Tran Tuan Son <trantuanson1991@gmail.com>
2024-05-10 18:04:29 +0700

    Add notification sound and badge as default.

--- src/Notification.php
+++ src/Notification.php
@@ -13,15 +13,28 @@ class Notification extends Base {
 
     public function setNotification($title, $message, $data = null) {
         $this -> validateCurrent($title, $message);
-        
-        $payload =  array(
-                    'notification' =>
-                    array(
-                        'title' => $title,
-                        'body' => $message
+
+        $payload = array(
+            'notification' => array(
+                'title' => $title,
+                'body' => $message
+            ),
+            'android' => array(
+                'notification' => array(
+                    'sound' => 'default',
+                    'notification_count' => 1,
+                )
+            ),
+            'apns' => array(
+                'payload' => array(
+                    'aps' => array(
+                        'sound' => 'default',
+                        'badge' => 1
                     )
+                )
+            )
         );
-        
+
         if (is_array($data) && count($data)>0){
             $payload['data'] = $data; 
        }
